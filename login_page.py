d0faa288f42a5c62e8fc438ddd6ae87d790eebd3
siddheshkoshti <61538670+siddheshkoshti@users.noreply.github.com>
2025-06-09 23:12:26 +0530

    Update login_page.py
    import path updated

--- login_page.py
+++ login_page.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from pages.base_page import BasePage
+from pythonProject1.pages.base_page import BasePage
 
 class LoginPage(BasePage):
     def login(self, email, password):
